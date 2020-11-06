b2c0147d808ab563e05a0ea33ac611b11b74e769
Ryan Mueller <55604547+ryanmueller28@users.noreply.github.com>
2020-11-05 19:12:28 -0600

    C# Solution to a string reversal problem

---
+++ CSharp/String-Reversal/String-Reversal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashString
+{
+    class Games
+    {
+        static void Main(string[] args)
+        {
+            string CheckString = Console.ReadLine();
+
+
+            string PrintString = ReverseString(CheckString);
+
+            Console.WriteLine(PrintString);
+        }
+
+        /**
+        * @param - InString, the string to be passed in 
+        * takes a string and separates out non-digit and non-alpha characters
+        * keeping those indexed in a dictionary
+        * And the Alpha/Numeric in an ArrayList (Vector for my cpp mind)
+        */
+        static string ReverseString(string InString)
+        {
+            string OutString = "";
+
+            Dictionary<int, char> SpecialIndex = new Dictionary<int, char>();
+            
+            ArrayList NormalChars = new ArrayList();
+
+            // Set up indexes and dictionary of special characters
+            for (int i = 0; i < InString.Length; i++)
+            {
+                if (!Char.IsLetter(InString[i]) && !Char.IsNumber(InString[i]))
+                {
+                    SpecialIndex.Add(i, InString[i]);
+                }else{
+                    NormalChars.Add(InString[i]);
+                }
+            }
+
+            NormalChars.Reverse();
+
+            
+
+            for (int i = 0; i < InString.Length; i++)
+            {
+                if (SpecialIndex.ContainsKey(i))
+                {
+                    OutString += SpecialIndex[i]; // If index is i, add taht
+                }else{
+                    OutString += NormalChars[0]; // add first element in NormalChars and remove first element.
+                    NormalChars.RemoveAt(0);
+                }
+            }
+
+            return OutString;
+        }
+    }
+}
